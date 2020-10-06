0a30d3f840d8a6ec473cefaa2100d3b7576cb949
Ion Dodon <iondodon2@gmail.com>
2020-10-06 13:42:45 +0300

    dev end: vscode

--- Lab1/gateway/lib/gateway/router.ex
+++ Lab1/gateway/lib/gateway/router.ex
@@ -2,27 +2,29 @@ defmodule Gateway.Router do
   use Plug.Router
   alias Gateway.Service
 
-  plug Plug.Parsers, parsers: [:urlencoded, :json], pass: ["text/*"], json_decoder: Jason
-  plug :match
-  plug :dispatch
+  plug(Plug.Parsers, parsers: [:urlencoded, :json], pass: ["text/*"], json_decoder: Jason)
+  plug(:match)
+  plug(:dispatch)
 
   get "/hello" do
     case Service.get(conn.request_path) do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
         send_resp(conn, 200, body)
+
       {:ok, %HTTPoison.Response{status_code: 404}} ->
         send_resp(conn, 404, "404. not found")
+
       {:error, %HTTPoison.Error{reason: reason}} ->
         send_resp(conn, 500, reason)
     end
   end
 
   post "/menus" do
-    ops =  [{"Content-Type", "application/json"}]
+    ops = [{"Content-Type", "application/json"}]
     {:ok, body} = Jason.encode(conn.body_params)
     {:ok, response} = Service.post("/menus", body, ops)
     {:ok, encoded_body} = Jason.encode(response.body)
-    send_resp(conn, 201, response.body)
+    send_resp(conn, 201, encoded_body)
   end
 
   match _ do
--- Lab1/gateway/lib/gateway/supervisor.ex
+++ Lab1/gateway/lib/gateway/supervisor.ex
@@ -1,13 +1,12 @@
 defmodule Gateway.Supervisor do
-  @moduledoc false
-
   use Supervisor
 
+  @spec start_link :: :ignore | {:error, any} | {:ok, pid}
   def start_link do
     Supervisor.start_link(__MODULE__, [])
   end
 
-  def init(arg) do
+  def init(_) do
     children = [
       {Plug.Cowboy, scheme: :http, plug: Gateway.Router, options: [port: 4000]}
     ]
