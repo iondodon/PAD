3327ece214194b861a5428f348841970067e1a31
Ion Dodon <iondodon2@gmail.com>
2020-10-18 15:49:40 +0300

    gateway work with two services now

--- Lab1/gateway/lib/gateway/circuit_breaker.ex
+++ Lab1/gateway/lib/gateway/circuit_breaker.ex
@@ -16,17 +16,17 @@ defmodule Service.CircuiBreaker do
         case HttpClient.request(method, url, body, headers) do
             {:ok, response} -> {:ok, response}
             {:error, err} -> 
-                update_service_state(service_address)
+                update_service_state(service, service_address)
                 {:error, err}
         end
     end
 
-    defp update_service_state(service_address)  do
+    defp update_service_state(service, service_address)  do
         failures = ECache.command("INCR #{cache_key(service_address)}")
 
         if failures >= @feilures_limit do
             ECache.command("DEL #{cache_key(service_address)}")
-            ECache.command("LREM services #{service_address}")
+            ECache.command("LREM #{service} #{service_address}")
         end
     end
 
