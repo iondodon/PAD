645938afb4e12299d7eebee196d5f180ca9b025f
Ion Dodon <iondodon2@gmail.com>
2020-11-23 16:00:34 +0200

    take first slave and first replica

--- Lab2/cache-master/lib/cache-master/command.ex
+++ Lab2/cache-master/lib/cache-master/command.ex
@@ -1,16 +1,22 @@
 defmodule Cache.Command do
     require Logger
-    alias Cache.Storage
+    alias Cache.SlaveRegistry
 
     @recv_length 0
+    @tag_replicas "replicas#"
 
     # Command execution on slave
     def run(command) do
-        slave_name = Storage.rpoplpush("slaves", "slaves")
-        slave = Storage.get(slave_name)
-        Logger.info("EXECUTE #{command} on slave #{Kernel.inspect(slave)}")
-        :gen_tcp.send(slave, command)
-        {:ok, response_from_slave} = :gen_tcp.recv(slave, @recv_length)
+        registry = SlaveRegistry.get_registry()
+
+        [first_slave_name | _tail] = Map.get(registry, "slaves", [])
+        [first_replica_socket | _tail] = Map.get(registry, @tag_replicas <> first_slave_name)
+
+
+        Logger.info("EXECUTE #{command} on slave #{Kernel.inspect(first_replica_socket)}")
+        :gen_tcp.send(first_replica_socket, command)
+
+        {:ok, response_from_slave} = :gen_tcp.recv(first_replica_socket, @recv_length)
         Logger.info(response_from_slave)
         response_from_slave
     end
