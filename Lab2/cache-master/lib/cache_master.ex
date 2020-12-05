79661bdbfa1d843703622785f822101967c58d70
Ion Dodon <iondodon2@gmail.com>
2020-12-05 15:15:30 +0200

    cache v3 - default master

--- Lab2/cache-master/lib/cache_master.ex
+++ Lab2/cache-master/lib/cache_master.ex
@@ -2,6 +2,11 @@ defmodule CacheMaster do
 	use Application
 
 	def start(_type, _args) do
-		Cache.BaseSupervisor.run_as_slave()
+		is_default_master = System.get_env("DEFAULT_MASTER")
+		if is_default_master == "true" do
+			Cache.BaseSupervisor.run_as_master()
+		else
+			Cache.BaseSupervisor.run_as_slave()
+		end
 	end
 end
